7141876011d31829e7c3fcd80074634d826bd4d8
pvaidy-icims <pvaidy@icims.com>
2025-05-29 14:43:20 +0530

    fix for sonar warnings

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 import { AuthService } from '../auth.service';
 import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-import { ToastrModule, ToastrService } from 'ngx-toastr';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 
@@ -20,7 +18,7 @@ import { HttpClientModule } from '@angular/common/http';
 })
 export class LoginComponent {
   loginUserData:any = {}
-  constructor(private _auth: AuthService, private _router: Router){}
+  constructor(private readonly _auth: AuthService, private readonly _router: Router){}
 
 
   loginUser(){
@@ -28,7 +26,6 @@ export class LoginComponent {
       console.log(res)
       localStorage.setItem('token', res.token)
       this._router.navigate(['/special'])
-     // this.toastr.error('Hello world!', 'Toastr fun!');
     },
   (err: any) => console.log(err))
   }
